4b55bba8b2604505800ac3955467f91694e94448
Prasandeep <prasandeepp@gmail.com>
2023-08-22 00:52:15 +0530

    transaction, commmit and rollback added!

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,10 +1,12 @@
 const Expense = require('../models/Expense');
 const User = require('../models/User');
+const sequelize = require('../db/database');
 
 const addExpense = async (req, res) => {
   const userId = req.user.id;
   const { amount, details, category } = req.body;
-  console.log("Categry:", category);
+
+  const t = await sequelize.transaction(); 
 
   try {
     const expense = await Expense.create({
@@ -12,17 +14,22 @@ const addExpense = async (req, res) => {
       details,
       category,
       UserId: userId 
+    }, { transaction: t });
+
+    await User.increment('totalExpense', {
+      by: parseFloat(amount),
+      where: { id: userId },
+      transaction: t
     });
-      await User.increment('totalExpense', {
-        by: parseFloat(amount),
-        where: { id: userId }
-      });
-    
-      return res.status(201).json({ success: true, expense });
-    } catch (error) {
-      console.error('Error incrementing totalExpense:', error);
-      return res.status(500).json({ success: false, message: 'An error occurred' });
-    }
+
+    await t.commit(); 
+
+    return res.status(201).json({ success: true, expense });
+  } catch (error) {
+    console.error('Error adding expense:', error);
+    await t.rollback(); 
+    return res.status(500).json({ success: false, message: 'An error occurred' });
+  }
 };
 
 const getExpenses = async (req, res) => {
@@ -37,17 +44,31 @@ const getExpenses = async (req, res) => {
 };
 
 const deleteExpense = async (req, res) => {
-
   const userId = req.user.id;
   const { id } = req.body;
+
+  const t = await sequelize.transaction();
+
   try {
-    const expense = await Expense.findOne({ where: { id, UserId: userId } });
+    const expense = await Expense.findOne({ where: { id, UserId: userId }, transaction: t });
     if (!expense) {
+      await t.rollback(); 
       return res.status(404).json({ success: false, message: 'Expense not found' });
     }
-    await expense.destroy();
+
+    await User.decrement('totalExpense', {
+      by: parseFloat(expense.amount),
+      where: { id: userId },
+      transaction: t
+    });
+
+    await expense.destroy({ transaction: t });
+    await t.commit();
+
     return res.status(200).json({ success: true });
   } catch (error) {
+    console.error('Error deleting expense:', error);
+    await t.rollback(); 
     return res.status(500).json({ success: false, message: 'An error occurred' });
   }
 };
