c301f3420c0363f038eb6df8cb661223c18b46c4
Prasandeep <prasandeepp@gmail.com>
2023-08-27 21:00:45 +0530

    getting error while reseting password!

--- views/reset.js
+++ views/reset.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
   
       const newPassword = resetPasswordForm.newPassword.value;
       console.log("New Password: ", newPassword);
-      const requestId = window.location.pathname.split('/').pop(); // Extract requestId from URL
+      const requestId = window.location.pathname.split('/').pop();
   
       try {
-        const response = await axios.put(`/reset-password/${requestId}`, { newPassword }); // Updated route path
+        const response = await axios.put(`/reset-password/${requestId}`, { newPassword }); 
         if (response.data.message) {
           alert(response.data.message);
-          window.location.href = '/login'; // Redirect to login page after password reset
+          window.location.href = '/login'; 
         }
       } catch (error) {
         console.error('Error resetting password:', error);
